783893b01fceaf2bd954008f4fa8d6642cb82fd9
azinza <anh.lh@zinza.com.vn>
2022-06-03 09:34:14 +0700

    creater,update,remove user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Body, Post, Get } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { User } from './type';
 import { CreateUserDto } from './user.dto';
 import { UserService } from './user.service';
-
+import { UpdateUserDto } from './update_user.dto';
 @Controller('user')
 export class UserController {
   constructor(private readonly UserService: UserService) {}
   @Get()
   async findOne(): Promise<User> {
     return;
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.UserService.remove(+id);
+  }
+  @Patch('/:id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.UserService.update(+id, updateUserDto);
   }
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import User from './user.entity';
-// import { CreateUserDto } from './user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
+import { UpdateUserDto } from './update_user.dto';
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.userRepository.findOneBy({ id });
+    Object.assign(user, updateUserDto);
+    return await this.userRepository.save(user);
+  }
   async createUser(user: User) {
     const newUser = this.userRepository.create(user);
     return await this.userRepository.save(newUser);
+  }
+  async remove(id: number) {
+    const user = await this.userRepository.findOneBy({ id });
+    return await this.userRepository.remove(user);
   }
 }
