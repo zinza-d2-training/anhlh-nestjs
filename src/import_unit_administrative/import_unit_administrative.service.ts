ee21756cedd6c8ed6c74a31e8a0f12a2fe8a3e9d
azinza <anh.lh@zinza.com.vn>
2022-06-09 15:10:25 +0700

    fix file import_unit_administrative

--- src/import_unit_administrative/import_unit_administrative.service.ts
+++ src/import_unit_administrative/import_unit_administrative.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Command, CommandArguments, _cli } from '@squareboat/nest-console';
-import Ward from 'src/entities/ward';
-import Province from 'src/entities/province';
-import District from 'src/entities/district';
+import { Command, _cli } from '@squareboat/nest-console';
+import Ward from 'src/entities/Ward';
+import Province from 'src/entities/Province';
+import District from 'src/entities/District';
 import { Repository } from 'typeorm';
 import * as XLSX from 'xlsx';
+
 @Injectable()
 export class ImportUnitAdministrativeService {
   constructor(
@@ -16,61 +17,61 @@ export class ImportUnitAdministrativeService {
     @InjectRepository(Province)
     private readonly provinceRepository: Repository<Province>,
   ) {}
-  @Command('export', {
-    desc: 'Export data from excel to mysql server',
+  @Command('import', {
+    desc: 'import data from excel to mysql server',
     args: { name: { req: false } },
   })
-  async sayHello(args: CommandArguments) {
-    const listProvince = [];
-    const listDistrict = [];
-    const listWard = [];
-    _cli.info(`Hello ${args.name || 'world'}!`);
+  async importFile() {
+    const provinces = [];
+    const districts = [];
+    const wards = [];
+    _cli.info(`import file`);
     const dataUnitAdministrative = XLSX.readFile(
       'src/utils/Danh sách cấp tỉnh kèm theo quận huyện, phường xã ___04_06_2022.xls',
     ).Sheets;
-    const listRecords = XLSX.utils.sheet_to_json(
+    const unitAdministratives = XLSX.utils.sheet_to_json(
       dataUnitAdministrative['Sheet1'],
     );
-    for (const record of listRecords) {
-      const isExistListProvince: boolean = listProvince.some(
-        (province) => province.name == record['Tỉnh Thành Phố'],
+    for (const province of unitAdministratives) {
+      const isExistListProvince: boolean = provinces.some(
+        (province) => province.name == province['Tỉnh Thành Phố'],
       );
       if (!isExistListProvince) {
-        listProvince.push({ name: record['Tỉnh Thành Phố'] });
+        provinces.push({ name: province['Tỉnh Thành Phố'] });
       }
     }
-    await this.provinceRepository.insert(listProvince);
+    await this.provinceRepository.insert(provinces);
 
     const listProvinceFromDB = await this.provinceRepository.find();
-    for (const record of listRecords) {
+    for (const district of unitAdministratives) {
       for (const province of listProvinceFromDB) {
-        if (province.name === record['Tỉnh Thành Phố']) {
-          const isExistedDistrict = listDistrict.some(
-            (district) => district.name === record['Quận Huyện'],
+        if (province.name === district['Tỉnh Thành Phố']) {
+          const isExistedDistrict = districts.some(
+            (district) => district.name === district['Quận Huyện'],
           );
           if (!isExistedDistrict) {
-            listDistrict.push({
-              name: record['Quận Huyện'],
+            districts.push({
+              name: district['Quận Huyện'],
               province_id: province.id,
             });
           }
         }
       }
     }
-    await this.districtRepository.insert(listDistrict);
+    await this.districtRepository.insert(districts);
 
     const listDistrictFromDB = await this.districtRepository.find();
-    for (const record of listRecords) {
+    for (const ward of unitAdministratives) {
       for (const district of listDistrictFromDB) {
-        if (district.name === record['Quận Huyện']) {
-          listWard.push({
-            name: record['Phường Xã'] || 'Undefined data',
+        if (district.name === ward['Quận Huyện']) {
+          wards.push({
+            name: ward['Phường Xã'] || 'Undefined data',
             district_id: district.id,
           });
         }
       }
     }
-    await this.wardRepository.insert(listWard);
+    await this.wardRepository.insert(wards);
     return;
   }
 }
