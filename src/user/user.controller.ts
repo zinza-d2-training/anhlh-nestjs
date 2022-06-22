889dd4b709b969a6894768dcac1ca56b142a270c
azinza <anh.lh@zinza.com.vn>
2022-06-22 21:43:54 +0700

    implement user api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Body, Post, Param, Patch, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Param,
+  Patch,
+  Delete,
+  Get,
+} from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './update_user.dto';
@@ -6,19 +14,17 @@ import { UpdateUserDto } from './update_user.dto';
 @Controller('user')
 export class UserController {
   constructor(private readonly UserService: UserService) {}
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.UserService.remove(+id);
-  }
 
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.UserService.findOne(id);
+  }
+  @Get()
+  findAll() {
+    return this.UserService.findAll();
+  }
   @Patch('/:id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return await this.UserService.update(+id, updateUserDto);
-  }
-
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    const response = await this.UserService.createUser(createUserDto);
-    return response;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,8 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UpdateUserDto } from './update_user.dto';
 import User from 'src/entities/User';
-import { CreateUserDto } from './create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -13,21 +12,13 @@ export class UserService {
   ) {}
 
   async findOne(email: string) {
-    return await this.userRepository.findOne({ email });
+    return await this.userRepository.findOne({ where: { email } });
   }
-
+  async findAll() {
+    return await this.userRepository.find();
+  }
   async update(id: number, updateUserDto: UpdateUserDto) {
     const user = await this.userRepository.findOne({ id });
-    Object.assign(user, updateUserDto);
-    return await this.userRepository.save(user);
-  }
-
-  async createUser(body: CreateUserDto) {
-    return this.userRepository.create(body);
-  }
-
-  async remove(id: number) {
-    const user = await this.userRepository.findOne({ id });
-    return await this.userRepository.remove(user);
+    return await this.userRepository.update(user, updateUserDto);
   }
 }
