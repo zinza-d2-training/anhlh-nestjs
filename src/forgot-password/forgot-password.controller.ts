2122670b137cb2c230fd1f10c55d199c09f1d61f
azinza <anh.lh@zinza.com.vn>
2022-06-24 15:47:57 +0700

    fix file forgot-pw

--- src/forgot-password/forgot-password.controller.ts
+++ src/forgot-password/forgot-password.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Get, Body, Query } from '@nestjs/common';
 import { ForgotPasswordService } from './forgot-password.service';
 
-@Controller('/forgot-passwords')
+@Controller('/forgot-password')
 export class ForgotPasswordController {
   constructor(private forgotPasswordService: ForgotPasswordService) {}
   @Post('/')
@@ -9,7 +9,7 @@ export class ForgotPasswordController {
     return await this.forgotPasswordService.sendUserConfirmation(email);
   }
 
-  @Get('/confirms')
+  @Get('/confirm')
   async restPassword(@Query('token') token: string) {
     return await this.forgotPasswordService.restPassword(token);
   }
